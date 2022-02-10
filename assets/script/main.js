e6d490efee3db30544d46d1078061c9d95eb30c3
BillyKybe <billykibet@gmail.com>
2022-02-10 04:46:08 +0300

    Init Push

---
+++ assets/script/main.js
@@ -0,0 +1,6 @@
+const el = document.querySelector(".container");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
